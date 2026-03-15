411b7f3879ba3dc9e7be873815e3eeba1c047107
MingYang Gao <wangfugui12-11@outlook.com>
2026-03-15 20:15:02 +0800

    Create IdentityVerification.java

---
+++ IdentityVerification.java
@@ -0,0 +1,18 @@
+/**
+ * Student Identification Script
+ * Created on March 15, 2026
+ */
+public class IdentityVerification {
+    public static void main(String[] args) {
+        String studentName = "Mingyang Gao"; //
+        String college = "Zhangjiakou Vocational and Technical College"; //
+        String major = "Software Technology"; //
+        String graduation = "June 30, 2028"; //
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("School: " + college);
+        System.out.println("Major: " + major);
+        System.out.println("Expected Graduation Date: " + graduation);
+        System.out.println("Software technology learning in progress...");
+    }
+}
